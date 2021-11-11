fd8337dd6a805ba0d708645890f3e26fa1a3e495
Ewerthon Kawan Gomes Ramalho <plantaseanimais07@gmail.com>
2021-11-11 17:29:34 -0300

    CustomHead component added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,13 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children }: Props) => (
   <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
     <header>
       <nav>
         <Link href="/">
