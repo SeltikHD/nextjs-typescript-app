a43491ce10cae971ce4591f5f9f34576a1088168
Ewerthon Kawan Gomes Ramalho <plantaseanimais07@gmail.com>
2021-11-25 22:12:23 -0300

    Changes on CustomHead.tsx

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -59,7 +59,7 @@ export default function customHead({
             <meta name="keywords" content={finalKeywords} />
             <meta name="robots" content={robots} />
             <meta name="author" content={orgName} />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="favicon.ico" />
             <meta name="description" content={description} />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
@@ -69,48 +69,48 @@ export default function customHead({
             <meta name="msapplication-TileColor" content={themeColor} />
             <meta name="msapplication-starturl" content={url} />
 
-            <meta name="msapplication-TileImage" content={iconsPath + '/favicon.ico'} />
-            <link rel="shortcut icon" href={iconsPath + '/favicon.ico'} type="image/x-icon" />
-            <link rel="apple-touch-icon" href={iconsPath + '/apple-touch-icon/apple-touch-icon.png'} />
+            <meta name="msapplication-TileImage" content={iconsPath + 'favicon.ico'} />
+            <link rel="shortcut icon" href={iconsPath + 'favicon.ico'} type="image/x-icon" />
+            <link rel="apple-touch-icon" href={iconsPath + 'apple-touch-icon/apple-touch-icon.png'} />
             <link
                 rel="apple-touch-icon"
                 sizes="57x57"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-57x57.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-57x57.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="72x72"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-72x72.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-72x72.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="76x76"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-76x76.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-76x76.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="114x114"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-114x114.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-114x114.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="120x120"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-120x120.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-120x120.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="144x144"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-144x144.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-144x144.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="152x152"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-152x152.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-152x152.png'}
             />
             <link
                 rel="apple-touch-icon"
                 sizes="180x180"
-                href={iconsPath + '/apple-touch-icon/apple-touch-icon-180x180.png'}
+                href={iconsPath + 'apple-touch-icon/apple-touch-icon-180x180.png'}
             />
 
             <meta property="og:title" content={title} />
